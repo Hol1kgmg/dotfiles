c52e0c7818b22916e564203f291e4b0c3090c3c0
Hol1kgmg <101860122+Hol1kgmg@users.noreply.github.com>
2025-12-24 06:58:20 +0900

    fix:メンテナンスされていないclaude codeから移行

--- home/modules/editor/config/neovim/configs/lua/config/keymaps/ai.lua
+++ home/modules/editor/config/neovim/configs/lua/config/keymaps/ai.lua
@@ -3,6 +3,10 @@
 local keymap = vim.keymap
 
 -- Claude Code
-keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<CR>", { desc = "Toogle Claude Code" })
-
+keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<CR>", { desc = "toggle Claude Code" })
+keymap.set("n", "<leader>cf", "<cmd>ClaudeCodeFocus<CR>", { desc = "focus Claude Code" })
+keymap.set("n", "<leader>cm", "<cmd>ClaudeCodeSelectModel<CR>", { desc = "select Model Claude Code" })
+keymap.set("v", "<leader>cs", "<cmd>ClaudeCodeSend<CR>", { desc = "send selection to Claude" })
+keymap.set("n", "<leader>ca", "<cmd>ClaudeCodeDiffAccept<CR>", { desc = "accept Claude diff" })
+keymap.set("n", "<leader>cd", "<cmd>ClaudeCodeDiffDeny<CR>", { desc = "deny Claude diff" })
 
