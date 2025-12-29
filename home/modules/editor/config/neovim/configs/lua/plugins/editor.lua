ac66793c365d99e0e0317f83cdf06549c5040661
Hol1kgmg <101860122+Hol1kgmg@users.noreply.github.com>
2025-12-29 11:28:15 +0900

    feat:mini.commentとmini.surroundの追加

--- home/modules/editor/config/neovim/configs/lua/plugins/editor.lua
+++ home/modules/editor/config/neovim/configs/lua/plugins/editor.lua
@@ -55,6 +55,29 @@ return {
     },
   },
 
+  -- コメント操作
+  -- Docs: https://github.com/echasnovski/mini.comment
+  -- gcc: 行コメントトグル
+  -- gc{motion}: モーション範囲をコメント
+  -- ビジュアルモードでも動作
+  {
+    'echasnovski/mini.comment',
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  -- 囲み操作
+  -- Docs: https://github.com/echasnovski/mini.surround
+  -- sa{motion}{char}: 囲みを追加
+  -- sd{char}: 囲みを削除
+  -- sr{old}{new}: 囲みを置換
+  -- sf/sF/sh/sn: 囲みを検索
+  {
+    'echasnovski/mini.surround',
+    event = "VeryLazy",
+    opts = {},
+  },
+
   -- 補完エンジン
   -- Docs: https://cmp.saghen.dev/
   -- Note: buildフィールドを削除し、GitHubリリース版のプリビルドバイナリを使用
