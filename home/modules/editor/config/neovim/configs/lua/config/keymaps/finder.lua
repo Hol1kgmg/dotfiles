e594375d0280a1fcda14dd4199fa1c046279878b
Hol1kgmg <101860122+Hol1kgmg@users.noreply.github.com>
2026-01-08 09:03:11 +0900

    feat:エクスプローラーをmini.filesからoil.nvimに移行。lspを使ったファイル名変更反映の機能のため

--- home/modules/editor/config/neovim/configs/lua/config/keymaps/finder.lua
+++ home/modules/editor/config/neovim/configs/lua/config/keymaps/finder.lua
@@ -2,28 +2,32 @@
 
 local keymap = vim.keymap
 
--- ファイラー（mini.files）
-keymap.set("n", "<leader>e", function()
-  require("mini.files").open(vim.api.nvim_buf_get_name(0))
-end, { desc = "open filer (this file)" })
+-- -- ファイラー（mini.files）
+-- keymap.set("n", "<leader>e", function()
+--   require("mini.files").open(vim.api.nvim_buf_get_name(0))
+-- end, { desc = "open filer (this file)" })
+--
+-- keymap.set("n", "<leader>E", function()
+--   require("mini.files").open()
+-- end, { desc = "open filer (cwd)" })
+--
+-- vim.api.nvim_create_autocmd('User', {
+--   pattern = 'MiniFilesBufferCreate',
+--   callback = function(args)
+--     local buf_id = args.data.buf_id
+--     local MiniFiles = require('mini.files')
+--
+--     keymap.set('n', 'J', 'j', { buffer = buf_id, desc = "Move down" })
+--     keymap.set('n', 'K', 'k', { buffer = buf_id, desc = "Move up" })
+--
+--     keymap.set('n', '<Tab>', MiniFiles.go_in, { buffer = buf_id, desc = "Go in (enter dir)" })
+--     keymap.set('n', '<S-Tab>', MiniFiles.go_out, { buffer = buf_id, desc = "Go out (parent dir)" })
+--   end,
+-- })
 
-keymap.set("n", "<leader>E", function()
-  require("mini.files").open()
-end, { desc = "open filer (cwd)" })
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'MiniFilesBufferCreate',
-  callback = function(args)
-    local buf_id = args.data.buf_id
-    local MiniFiles = require('mini.files')
-
-    keymap.set('n', 'J', 'j', { buffer = buf_id, desc = "Move down" })
-    keymap.set('n', 'K', 'k', { buffer = buf_id, desc = "Move up" })
-
-    keymap.set('n', '<Tab>', MiniFiles.go_in, { buffer = buf_id, desc = "Go in (enter dir)" })
-    keymap.set('n', '<S-Tab>', MiniFiles.go_out, { buffer = buf_id, desc = "Go out (parent dir)" })
-  end,
-})
+-- ファイラー(oil.nvim)
+keymap.set("n", "<leader>e", "<cmd>Oil --float<cr>", { desc = "open filer(this files)"})
+keymap.set("n", "<leader>E", "<cmd>Oil . --float<cr>", { desc = "open filer (cwd)" })
 
 -- Fuzzy Finder（fff.nvim）
 keymap.set("n", "<leader>ff", "<cmd>FFFFind<CR>", { desc = "fzf" })
--- home/modules/editor/config/neovim/configs/lua/plugins/navigation.lua
+++ home/modules/editor/config/neovim/configs/lua/plugins/navigation.lua
@@ -1,28 +1,96 @@
 -- ファイル操作・検索
 return {
   -- ファイラー
+  -- {
+  --   "echasnovski/mini.files",
+  --   version = false,
+  --   dependencies = { "echasnovski/mini.icons" },
+  --   config = function()
+  --     require("mini.files").setup({
+  --       -- カスタマイズオプション
+  --       mappings = {
+  --         go_in = '',  -- lを無効化してカーソル移動として使用
+  --         go_out = '', -- hを無効化してカーソル移動として使用
+  --       },
+  --       options = {
+  --         use_as_default_explorer = true,
+  --       },
+  --       windows = {
+  --         preview = true,
+  --         width_focus = 30,
+  --         width_nofocus = 15,
+  --         width_preview = 50,
+  --       },
+  --     })
+  --   end,
+  -- },
   {
-    "echasnovski/mini.files",
-    version = false,
+    "stevearc/oil.nvim",
     dependencies = { "echasnovski/mini.icons" },
-    config = function()
-      require("mini.files").setup({
-        -- カスタマイズオプション
-        mappings = {
-          go_in = '',  -- lを無効化してカーソル移動として使用
-          go_out = '', -- hを無効化してカーソル移動として使用
+    config = function ()
+      require("oil").setup({
+        is_hidden_file = function (name)
+          return name:match("^%.")
+          or name == "DS_Store"
+          or name == "Thumbs.db"
+          or name:match("%.swp$")
+          or name:match("~$")
+        end,
+
+        lsp_file_methods = {
+          enabled = true,
+          timeout_ms = 1000,
+          autosave_changes = true,
         },
-        options = {
-          use_as_default_explorer = true,
+
+        use_default_keymaps = false,
+        keymaps = {
+          ["g?"] = { "actions.show_help", mode = "n" },
+          ["L"] = { "actions.select", mode = "n" },
+          ["H"] = { "actions.parent", mode = "n" },
+          ["<Tab>"] = { "actions.select", mode = "n" },
+          ["<S-Tab>"] = { "actions.parent", mode = "n" },
+          ["<C-t>"] = { "actions.select", opts = { tab = true } },
+          ["<C-p>"] = { "actions.preview", mode = "n" },
+          ["q"] = { "actions.close", mode = "n" },
+          ["<C-r>"] = "actions.refresh",
+          ["pwd"] = { "actions.open_cwd", mode = "n" },
+          ["cd"] = { "actions.cd", mode = "n" },
+          ["tcd"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
         },
-        windows = {
-          preview = true,
-          width_focus = 30,
-          width_nofocus = 15,
-          width_preview = 50,
+
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = "oil",
+          callback = function (args)
+            vim.keymap.set("n", "J", "j", { buffer = args.buf, desc = "move down"} )
+            vim.keymap.set("n", "K", "k", { buffer = args.buf, desc = "move up"} )
+          end,
+        }),
+
+
+        float = {
+          padding = 1,
+          -- max_width = 0.8,
+          -- max_height = 0.8,
+          border = "single",
+
+          preview_split = "right",
+
+          get_win_title = function ()
+            local oil = require("oil")
+            local cwd = oil.get_current_dir()
+            local root = vim.fn.getcwd()
+            local relpath = "."
+            if cwd ~= root and cwd:find(root, 1, true) == 1 then
+              relpath = cwd:sub(#root + 2)
+            elseif cwd ~= root then
+              relpath = cwd
+            end
+            return "[OIL] " .. relpath
+          end,
         },
       })
-    end,
+    end
   },
 
   -- Fuzzy Finder（fff.nvim）
