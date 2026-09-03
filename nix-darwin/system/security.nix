9e4f040f131fc17db7c600ebce89348061512765
Hol1kgmg <101860122+Hol1kgmg@users.noreply.github.com>
2026-09-03 21:03:48 +0900

    fix:tmux向けの設定を更新

--- nix-darwin/system/security.nix
+++ nix-darwin/system/security.nix
@@ -1,4 +1,6 @@
 {
   # sudoコマンドをtouchIDで実行許可
   security.pam.services.sudo_local.touchIdAuth = true;
+  # tmux内からのsudoでもtouchIDを使えるようにする（bootstrapセッションへの再アタッチ）
+  security.pam.services.sudo_local.reattach = true;
 }
