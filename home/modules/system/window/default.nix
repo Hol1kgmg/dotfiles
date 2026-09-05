a8995c0690163f4c8d8e22b8960ff93403ca349c
Hol1kgmg <101860122+Hol1kgmg@users.noreply.github.com>
2026-09-05 10:17:50 +0900

    fix:vicinae不採用にともないtrexを削除

--- home/modules/system/window/default.nix
+++ home/modules/system/window/default.nix
@@ -1,11 +1,11 @@
 {
   imports = [
     ./rectangle.nix
-    # ./autoraise.nix
-    # ./alt-tab-macos.nix
+    # ./autoraise.nix # 不具合があり、長期でメンテされてないため削除
+    # ./alt-tab-macos.nix # 有料プランによって使いづらくなった
     ./dockdoor.nix
     ./scroll-reverser.nix
-    ./trex.nix
+    # ./trex.nix # vicinaeで使用
     # ./hammerspoon.nix
   ];
 }
