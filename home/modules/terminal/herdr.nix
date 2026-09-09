74fe6eb26ed28ae26517a97969d7f86363ee0459
Hol1kgmg <abdevs.horisho@icloud.com>
2026-09-10 01:51:08 +0900

    feat:herdr upgrade v0.9.0

--- home/modules/terminal/herdr.nix
+++ home/modules/terminal/herdr.nix
@@ -1,63 +1,8 @@
-{ pkgs, lib, ... }:
+# herdr: ターミナルマルチプレクサ（パッケージ定義はnur-packagesで管理）
+{ inputs, pkgs, ... }:
 
-let
-  src = pkgs.fetchFromGitHub {
-    owner = "ogulcancelik";
-    repo = "herdr";
-    rev = "v0.8.2";
-    hash = "sha256-sEGIN3dLZasaHob3EHscWBCIQHflMQVchYmzgsETDk4=";
-  };
-
-  zigDeps = pkgs.callPackage "${src}/vendor/libghostty-vt/build.zig.zon.nix" {
-    name = "herdr-libghostty-vt-zig-cache";
-    inherit (pkgs) zstd;
-    linkFarm =
-      name: entries:
-      pkgs.runCommand name { } ''
-        mkdir -p $out
-        ${lib.concatMapStringsSep "\n" (entry: ''
-          cp -rL ${entry.path} $out/${entry.name}
-        '') entries}
-      '';
-  };
-in
 {
-  home.packages = [
-    (pkgs.rustPlatform.buildRustPackage {
-      pname = "herdr";
-      version = "0.8.2";
-      inherit src;
-
-      cargoLock = {
-        lockFile = "${src}/Cargo.lock";
-      };
-
-      nativeBuildInputs =
-        with pkgs;
-        [
-          git
-          pkg-config
-        ]
-        ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-          pkgs.cctools
-          pkgs.xcbuild
-        ];
-
-      env = {
-        LIBGHOSTTY_VT_OPTIMIZE = "ReleaseFast";
-        LIBGHOSTTY_VT_SIMD = "true";
-        LIBGHOSTTY_VT_ZIG_SYSTEM_DIR = zigDeps;
-        ZIG = lib.getExe pkgs.zig_0_15;
-      };
-
-      preBuild = ''
-        export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-global-cache"
-        export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-local-cache"
-      '';
-
-      doCheck = false;
-    })
-  ];
+  home.packages = [ inputs.nur-packages.packages.${pkgs.stdenv.hostPlatform.system}.herdr ];
 
   xdg.configFile."herdr/config.toml".source = ./configs/herdr/config.toml;
 }
